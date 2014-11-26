6d870611491d9d6f462a7dd7f56c9cc5f4867e93
ismaeelsameed <ismaeelsameed@hotmail.com>
2014-11-26 17:31:08 +0200

    design

--- aldaleel/views.py
+++ aldaleel/views.py
@@ -3,5 +3,5 @@ from django.template import RequestContext
 
 
 # Create your views here.
-def home(request, template_name='page-homepage-sample.html'):
+def home(request, template_name='index.html'):
     return render_to_response(template_name, context_instance=RequestContext(request))
