ec07a02aaf7b1fa543e7c2dd52adacef3c142637
ismaeelsameed <ismaeelsameed@hotmail.com>
2014-12-07 00:37:28 +0200

    design

--- aldaleel/views.py
+++ aldaleel/views.py
@@ -5,3 +5,7 @@ from django.template import RequestContext
 # Create your views here.
 def home(request, template_name='index.html'):
     return render_to_response(template_name, context_instance=RequestContext(request))
+
+
+def companies(request, template_name='page-portfolio-3-columns-1.html'):
+    return render_to_response(template_name, context_instance=RequestContext(request))
