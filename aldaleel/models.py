ed78cafc4c8323eede725d5a67f9e7b3a5408615
ismaeelsameed <ismaeelsameed@hotmail.com>
2014-12-27 15:10:04 +0200

    models changes

--- aldaleel/models.py
+++ aldaleel/models.py
@@ -17,9 +17,25 @@ class Company(models.Model):
     long_description = models.TextField()
     logo = models.ImageField(upload_to='company_logos', null=True, blank=True)
     image = models.ImageField(upload_to='company_images')
+    website = models.CharField(max_length=255, default="", null=True, blank=True)
     category = models.ForeignKey(Category, null=True, blank=False)
     is_active = models.BooleanField(default=True)
     is_promoted = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.english_name
+
+
+class Project(models):
+    name = models.CharField(max_length=255, default="", null=True, blank=False)
+    address = models.CharField(max_length=255, default="", null=True, blank=False)
+    area = models.CharField(max_length=255, default="", null=True, blank=False)
+    phone = models.CharField(max_length=255, default="", null=True, blank=False)
+    image = models.ImageField(upload_to='company_images')
+    company = models.ForeignKey(Company, null=True, blank=False)
+    price = models.CharField(max_length=255, default="", null=True, blank=False)
+    notes = models.TextField()
+    is_approved = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
